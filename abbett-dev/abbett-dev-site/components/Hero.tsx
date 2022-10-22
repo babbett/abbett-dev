fa21c4d22504cf8619b44d6d0b414a4b0f21fa3f
Ben Abbett <bwabbett@gmail.com>
2022-10-22 01:58:26 -0400

    Add Cypress for testing. Start Hero

---
+++ abbett-dev/abbett-dev-site/components/Hero.tsx
@@ -0,0 +1,7 @@
+function Hero() {
+    return (
+            <h1 className="uppercase">Hello FUcker</h1>
+    );
+}
+
+export default Hero
--- abbett-dev/abbett-dev-site/pages/index.tsx
+++ abbett-dev/abbett-dev-site/pages/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Hero from '../components/Hero'
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
@@ -13,6 +15,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <Hero></Hero>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
