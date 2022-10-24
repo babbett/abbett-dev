c0a42d866700a5ebf008a2f110e23bd57acf2022
Ben Abbett <bwabbett@gmail.com>
2022-10-24 01:34:36 -0400

    fix url

--- abbett-dev/abbett-dev-site/components/ProfilePic.tsx
+++ abbett-dev/abbett-dev-site/components/ProfilePic.tsx
@@ -5,6 +5,7 @@ function ProfilePic() {
     return (
         <div className="basis-1/4 place-self-center sm:mt-10">
             <Image className="bg-black" src={myImage} alt="Picture of me"></Image>
+            <img src={myImage} alt="Picture of me"/>
         </div>
     );
 }
