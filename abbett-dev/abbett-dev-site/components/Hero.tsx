5682ebf56d118f2fc72c1344a32c028fa0dbb744
Ben Abbett <bwabbett@gmail.com>
2022-10-23 21:39:43 -0400

    Scrap auto-created index page. Add nav and footer

--- abbett-dev/abbett-dev-site/components/Hero.tsx
+++ abbett-dev/abbett-dev-site/components/Hero.tsx
@@ -1,6 +1,26 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import Nameplate from "./Nameplate"
+
 function Hero() {
+    // const subtitleArr = [".NET Developer", "Penis Man", "Lil Boy Ben", "The Benster"]
+
+    // function changeMessage() {
+        
+    // }
     return (
-            <h1 className="uppercase">Hello FUcker</h1>
+        <>
+            {/* <div className="px-10 pt-2 max-w-3xl mx-auto rounded-xl bg-blue-100 shadow-lg">
+                <h1 className="text-left text-7xl">Ben Abbett</h1>
+                <p>.NET Developer</p>
+            </div> */}
+            <div className="p-10 shadow-lg bg-gray-100 rounded-3xl flex flex-row">
+                <Nameplate/>
+                <div className="basis-1/4">
+                    <Image src="/../public/IMG_1790 copy.png" layout="intrinsic" height="100%" width="100%" alt="Picture of me"></Image>
+                </div>
+            </div>
+        </>
     );
 }
 
