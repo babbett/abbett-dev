31304675c90a211f57c020e4b1ccf481c95bb963
Ben Abbett <bwabbett@gmail.com>
2022-10-23 21:45:36 -0400

    Successful build.

--- abbett-dev/abbett-dev-site/components/Nameplate.tsx
+++ abbett-dev/abbett-dev-site/components/Nameplate.tsx
@@ -1,7 +1,7 @@
 function Nameplate() {
     return (
         <div className="flex flex-col basis-3/4">
-            <h1 className="text-center text-blue-600 text-8xl basis-2/3">Hi, I'm Ben Abbett!</h1>
+            <h1 className="text-center text-blue-600 text-8xl basis-2/3">Hi, I{"'"}m Ben Abbett!</h1>
             <h2 className="text-center basis-1/3 text-3xl">.NET Developer</h2>
         </div>
     );
