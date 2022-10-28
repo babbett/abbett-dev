518a926be6495f8de2e556b818127056eac2c1d5
Ben Abbett <bwabbett@gmail.com>
2022-10-28 16:47:59 -0400

    Add vercel analytics

--- abbett-dev/abbett-dev-site/pages/_app.tsx
+++ abbett-dev/abbett-dev-site/pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout';
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </>
   );
