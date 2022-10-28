161d29be6b1d7e90a7a37f5d3968a2509c69b063
Ben Abbett <bwabbett@gmail.com>
2022-10-28 16:42:54 -0400

    Change <a> to <Link>

--- abbett-dev/abbett-dev-site/components/Navbar.tsx
+++ abbett-dev/abbett-dev-site/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction, useState } from "react";
+import Link from "next/link";
 
 type NavProps = {
     to: string,
@@ -11,24 +12,26 @@ type MobileProps = {
 }
 
 function NavLink({ to, children }: NavProps) {
-    return <a href={to} className={`mx-4`}>
-        {children}
-    </a>
+    return (
+        <Link href={to} className="mx-4 border-4 border-blue-500">
+            {children}
+        </Link>
+    );
 }
 
 function MobileNav({ open, setOpen }: MobileProps) {
     return (
         <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
             <div className="flex items-center justify-center filter drop-shadow-md bg-white h-20"> {/*logo container*/}
-                <a className="text-xl font-semibold" href="/">LOGO</a>
+                <Link className="text-xl font-semibold" href="/">Logo</Link>
             </div>
             <div className="flex flex-col ml-4">
-                <a className="text-xl font-medium my-4" href="/about" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
-                    About
-                </a>
-                <a className="text-xl font-normal my-4" href="/contact" onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>
-                    Contact
-                </a>
+                <Link className="text-xl font-medium my-4" href="/about">
+                    <a onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>About</a>
+                </Link>
+                <Link className="text-xl font-normal my-4 ml-1" href="/contact">
+                    <a onClick={() => setTimeout(() => {setOpen(!open)}, 100)}>Contact</a>
+                </Link>
             </div>  
         </div>
     )
@@ -40,7 +43,7 @@ function Navbar() {
         <nav className="flex filter drop-shadow-md bg-white px-4 py-4 h-20 items-center">
             <MobileNav open={open} setOpen={setOpen}/>
             <div className="w-3/12 flex items-center">
-                <a className="text-2xl font-semibold" href="/">LOGO</a>
+                <Link className="text-2xl font-semibold" href="/">Logo</Link>
             </div>
             <div className="w-9/12 flex justify-end items-center">
 
