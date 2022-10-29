dead13213242356289dd0aebd21c4026954c7127
Ben Abbett <bwabbett@gmail.com>
2022-10-29 17:11:20 -0400

    Fix the basis on the profile pic

--- abbett-dev/abbett-dev-site/components/Nameplate.tsx
+++ abbett-dev/abbett-dev-site/components/Nameplate.tsx
@@ -1,6 +1,6 @@
 function Nameplate() {
     return (
-        <div className="flex flex-col gap-y-10 my-auto basis-3/4">
+        <div className="flex flex-col gap-y-10 my-auto basis-9/12">
             <h1 className="text-center text-blue- dark:text-white text-8xl">Hi, I{"'"}m Ben Abbett!</h1>
             <h2 className="text-center dark:text-white text-3xl">GAMER</h2>
         </div>
