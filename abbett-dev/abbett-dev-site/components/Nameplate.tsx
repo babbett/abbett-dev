1ee1ce0fcac08fe2eb4fc1b2539eac3005025262
Ben Abbett <bwabbett@gmail.com>
2022-11-12 09:47:24 -0500

    go ben mode

--- abbett-dev/abbett-dev-site/components/Nameplate.tsx
+++ abbett-dev/abbett-dev-site/components/Nameplate.tsx
@@ -3,7 +3,7 @@ function Nameplate() {
     //console.log(projArray)
     return (
         <div className="flex flex-col gap-y-10 my-auto basis-9/12">
-            <h1 className="text-center text-blue- dark:text-white lg:text-8xl md:text-5xl text-4xl">Hi, I{"'"}m Ben Abbett!</h1>
+            <h1 className="text-center text-blue- dark:text-white lg:text-8xl md:text-5xl text-4xl">go ben mode</h1>
             <h2 className="text-center dark:text-white text-3xl">peenis</h2>
         </div>
     );
