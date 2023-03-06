3eb0507a84b59463243774f778bce069bb38c9a6
Ben Abbett <ben@abbett.dev>
2023-03-06 00:56:31 -0500

    bunch of progress

--- abbett-dev/abbett-dev-site/components/ChatBox.tsx
+++ abbett-dev/abbett-dev-site/components/ChatBox.tsx
@@ -2,19 +2,27 @@ import { useState, useEffect } from "react";
 import { getDatabase, ref, set, onValue } from "firebase/database";
 import { randomUUID } from "crypto";
 
-import { MessageClass as Message, database } from "../pages/api/db";
+import { MessageClass as Message, database, getDateForDB } from "../pages/api/db";
 
 
 
 const ChatBox = () => {
     const [messages, setMessages] = useState<Message[]>([]);
+    const [username, setUsername] = useState<string>("");
 
     useEffect(() => {
+        // get current day for folder structure
+        const date = new Date(Date.now());
+
         const db = database;
-        const messagingRef = ref(db, "messaging/global/");
+        const messagingRef = ref(db, `messaging/global/${getDateForDB(date)}`);
 
         onValue(messagingRef, (snapshot) => {
             const data = snapshot.val();
+
+            if (data === null) {
+                return;
+            }
 
             const messagesResp: Message[] = Object.values(data);
 
@@ -22,66 +30,119 @@ const ChatBox = () => {
                 console.log(message);
             });
 
-            //addNewMessage(data);
             setMessages(() => [...messagesResp.filter((message) => message.messageId !== undefined && message.message.length > 0)
                                               .sort((a, b) => a.timestamp - b.timestamp)                  
                               ]);
-            //updateStarCount(postElement, data);
         });
     }, []);
 
     const sendMessage = () => {
         const message: string = (document.getElementById("messageBox") as HTMLInputElement).value;
-
         if (message.length === 0) {
             return;
         }
 
-        let messageObj = new Message(message, Date.now(), "test", "test" + Math.floor(Math.random() * 1000));
-        //messageObj.logMessage();
+        let messageObj = new Message({ message, username });
 
-        const db = getDatabase();
-        const messagingRef = ref(db, "messaging/global/" + messageObj.messageId);
-
-        console.log("writing message to db: " + messageObj.message)
-        set(messagingRef, messageObj);
-
-        // clear the message box
-        (document.getElementById("messageBox") as HTMLInputElement).value = "";
+        try {
+            messageObj.send("global");
+            
+            // clear the message box
+            (document.getElementById("messageBox") as HTMLInputElement).value = "";
+        }
+        catch (e) {
+            console.log(e);
+        }
     };
 
-    return (
-        <div className="grid grid-rows-6 gap-1 chat-box">
-            <h1 className="dark:text-white text-center text-3xl">Messages</h1>
-            <div className="row-span-4 overflow-y-auto px-2" id="messages">
-                {messages.map((message) => (
-                    <SingleMessage message={message} key={message.messageId} />
-                ))}
+    const addUsername = () => {
+        const username: string = (document.getElementById("usernameBox") as HTMLInputElement).value;
+        if (username.length === 0) {
+            return;
+        }
+
+        setUsername(() => username);
+    };
+
+    if (username === "") {
+        return (
+            <div className="w-fit chat-box no-username mx-auto mt-2 mb-2">
+                {/*enter a username*/}
+                <label htmlFor="usernameBox" className="dark:text-white text-3xl">Please enter a username: </label>
+                <div className="flex py-2 px-1">
+                        <input
+                            id="usernameBox"
+                            title="set username"
+                            type="text"
+                            name="username"
+                            defaultValue=""
+                            className="flex-auto"
+                            maxLength={20}
+                            onKeyDown = {(e) => {
+                                if (e.key === "Enter") {
+                                    addUsername();
+                                }
+                                // animate button?
+                            }}
+                        />
+                        <button type="button" className="flex-none ml-2 px-2 border-2 rounded-md dark:text-white" onClick={addUsername}>
+                            Set
+                        </button>
+                    </div>
             </div>
-            <div className="row h-10" id="addNewMessage">
-                <div className="flex">
-                    <input
-                        id="messageBox"
-                        title="send message"
-                        type="text"
-                        name="message"
-                        defaultValue=""
-                        className="flex-auto border-4 border-blue-400"
-                        maxLength={1000}
-                    />
-                    <button type="button" className="flex-none border-2 rounded-md border-blue-200 dark:text-white" onClick={sendMessage}>
-                        Send
-                    </button>
+        )
+    } else {
+        return (
+            <div className="flex-row chat-box">
+                <h1 className="flex-none dark:text-white text-center text-3xl">Messages</h1>
+                <div className="flex-auto row-span-4 overflow-y-auto px-2" id="messages">
+                    {messages.map((message) => (
+                        <SingleMessage message={message} key={message.messageId} />
+                    ))}
+                </div>
+                <div className="flex-none h-10" id="addNewMessage">
+                    <hr className="my-2"/>
+                    <div className="flex">
+                        <input
+                            id="messageBox"
+                            title="send message"
+                            type="text"
+                            name="message"
+                            defaultValue=""
+                            className="flex-auto"
+                            maxLength={1000}
+                            onKeyDown = {(e) => {
+                                if (e.key === "Enter") {
+                                    sendMessage();
+                                }
+                                // animate button?
+                            }}
+                        />
+                        <button type="button" className="flex-none border-2 rounded-md dark:text-white" onClick={sendMessage}>
+                            Send
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 const SingleMessage = (props: { message: Message }) => {
+    const UserName = (props: { className: string, children: Message }) => {
+        return <span className={props.className}>{props.children.username}</span>;
+    };
+
     return (
         <div className="dark:text-white mb-2 w-fit">
-            <p className="w-fit dark:border-white border-2 rounded-lg px-2 py-1">{props.message.username + ": " + props.message.message}</p>
+            <div className="flex">
+                <UserName className="flex-none px-2 py-1 border-2 rounded-l-lg dark:border-sky-300">
+                    {props.message}
+                </UserName>
+                <p className="flex-auto break-words w-fit dark:border-white border-r-2 border-y-2 rounded-r-lg px-2 py-1">
+                    {props.message.message}
+                </p>
+            </div>
             <p className="w-fit mr-2">{new Date(props.message.timestamp).toLocaleString()}</p>
         </div>
     );
--- abbett-dev/abbett-dev-site/pages/api/db.ts
+++ abbett-dev/abbett-dev-site/pages/api/db.ts
@@ -2,7 +2,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, child, get, set } from "firebase/database";
-import { randomUUID } from "crypto";
+import { parse, v4 as uuidv4 } from 'uuid';
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -51,11 +52,27 @@ export function getUserData() {
     });
 }
 
+/**
+ * The database stores the messages in a folder structure based on the date, 
+ * this function converts a date to the format used by the database (month-day-year)
+ * @param date The date to convert
+ * @returns An object containing the day, month, and year
+ */
+export function getDateForDB(date: Date) {
+    const day = date.getUTCDate();
+    const month = date.getUTCMonth() + 1;
+    const year = date.getUTCFullYear();
+    return `${month}-${day}-${year}`;
+}
+
 export interface Message {
     message: string;
     timestamp: number;
     username: string;
     messageId: string;
+
+    logMessage(): void;
+    send(destination: string): void;
 }
 
 export class MessageClass implements Message {
@@ -64,14 +81,74 @@ export class MessageClass implements Message {
     username: string;
     messageId: string;
 
-    constructor(message: string, timestamp: number, username: string, messageId: string) {
-        this.message = message;
-        this.timestamp = timestamp;
+    constructor({ message, username }: { message: string; username: string; }) {
+        if (message.length === 0) {
+            throw new Error("Message cannot be empty");
+        } else if (username.length === 0) {
+            throw new Error("Username cannot be empty");
+        }
+        
         this.username = username;
-        this.messageId = messageId;
+        this.message = message;
+        this.timestamp = Date.now();
+        this.messageId = uuidv4();
+        console.log(this.messageId)
     }
+
 
     logMessage() {
         console.log("The message: " + this.message + ", was sent at " + this.timestamp + " by " + this.username + " with id " + this.messageId);
+    }
+
+    // TODO: destination should probably be a enum or something, better than just a ambiguous string
+    // TODO: Maybe this should be a static method?
+    /** Sends the message to the destination
+     * @param destination The destination of the message
+     * @throws Error if the message fails to send
+     * 
+     * TODO: destination should probably be a enum or something, better than just a ambiguous string
+     */
+    send(destination: string) {
+        // Make sure it isn't empty
+        if (this.message.length === 0) {
+            return;
+        }
+
+        // Send the message
+        try {
+            // Day and is used to create a folder structure for the messages
+            const date = new Date(this.timestamp);
+
+            const db = getDatabase();
+            const messagingRef = ref(db, `messaging/${destination}/${getDateForDB(date)}/${this.messageId.toString()}`);
+    
+            console.log("writing message: " + this.message + " to " + destination);
+            set(messagingRef, this);
+        }
+        catch (error) {
+            console.error(`Failed to send message with ID: ${this.messageId} to ${destination}`);
+            throw new Error("Failed to send message");
+        }
+    }
+
+    /** Allows for a message to be sent without having to create a new instance of the class 
+     * 
+     * @param message The message to send
+     * @param username The username of the sender
+     * @param destination The destination of the message
+     * @throws Error if the message fails to send
+     */
+    static send(message: string, username: string, destination: string) {
+        // Create the message
+        const messageObj = new MessageClass({ message, username });
+
+        // Send it
+        try {
+            messageObj.send(destination);
+        }
+        catch (error) {
+            console.error(`Failed to send message with ID: ${messageObj.messageId} to ${destination}`);
+            throw new Error("Failed to send message");
+        }
     }
 }
